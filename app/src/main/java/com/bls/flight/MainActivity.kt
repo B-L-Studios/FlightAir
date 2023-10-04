02a8bb46eca7170396a2e44c2865f7eedafd5ec7
Louay labidi <61356202+louayyy99@users.noreply.github.com>
2023-10-04 09:33:48 +0100

    initial commit

---
+++ app/src/main/java/com/bls/flight/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.bls.flight
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.bls.flight.ui.theme.FlightComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlightComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
